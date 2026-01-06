ffd789f0710a8cee340e98f34c7419e7347bacd8
CALVIN <jeancalvin027@gmail.com>
2026-01-06 11:31:55 +0200

    Initial Commit

---
+++ FocusOS/FocusOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusOSApp.swift
+//  FocusOS
+//
+//  Created by calvin on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct FocusOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusOSTests/FocusOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  FocusOSTests.swift
+//  FocusOSTests
+//
+//  Created by calvin on 1/6/26.
+//
+
+import Testing
+@testable import FocusOS
+
+struct FocusOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
